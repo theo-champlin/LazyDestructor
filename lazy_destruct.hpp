47ae5a004fd4a676fab720cf02821674fc52fb5c
theo-champlin <40192128+theo-champlin@users.noreply.github.com>
2018-06-29 21:37:58 -0700

    Cleaning up the heap.
    Making a heap for each thread to avoid concurrency issues. Also clearing a heap on destruction to ensure the destructors have run by the time the thread exists.

--- lazy_destruct.hpp
+++ lazy_destruct.hpp
@@ -6,96 +6,106 @@
 
 #include <iostream>
 
-//TODO thread safety
 class deferred_heap
 {
 public:
-    struct element_information
-    {
-        element_information(
-           std::size_t size,
-           void (*deleter)(std::byte*) noexcept)
-           :
-           size{ size },
-           deleter{ deleter }
-        {}
+   struct element_information
+   {
+      element_information(
+         std::size_t size,
+         void (*deleter)(std::byte*) noexcept)
+         :
+         size{ size },
+         deleter{ deleter }
+      {}
 
-        std::size_t size;
-        void (*deleter)(std::byte*) noexcept;
+      std::size_t size;
+      void (*deleter)(std::byte*) noexcept;
 
-        std::size_t offset;
-    };
+      std::size_t offset;
+   };
 
-    template <std::size_t capacity = 512>
-    static deferred_heap& get()
-    {
-        static deferred_heap heap{ capacity };
-        return heap;
-    }
+   static deferred_heap& get()
+   {
+      thread_local deferred_heap heap;
+      return heap;
+   }
 
-    //TODO ensure noexcept
-    void enqueue(element_information info, std::byte* element)
-    {
-        info.offset = elements.empty() ? 0 : elements.back().offset + elements.back().size;
-        std::copy(element, element + info.size, std::begin(heap) + info.offset);
-        elements.emplace(std::move(info));
-    }
+   void enqueue(element_information info, std::byte* element)
+   {
+      info.offset = elements.empty() ? 0 : elements.back().offset + elements.back().size;
 
-    //TODO ensure noexcept
-    bool dequeue()
-    {
-        if (elements.empty()) return false;
+      if (info.offset + info.size > std::size(heap))
+      {
+         info.deleter(element);
+         return;
+      }
 
-        const auto& head_element = elements.front();
-        head_element.deleter(heap.data() + head_element.offset);
-        elements.pop();
-        return true;
-    }
+      std::copy(element, element + info.size, std::begin(heap) + info.offset);
+      elements.emplace(std::move(info));
+   }
 
-    void clear()
-    {
-        while(dequeue());
-    }
+   bool dequeue()
+   {
+      if (elements.empty())
+      {
+         return false;
+      }
+
+      const auto& head_element = elements.front();
+      head_element.deleter(heap.data() + head_element.offset);
+      elements.pop();
+
+      return true;
+   }
+
+   void clear()
+   {
+      while(dequeue());
+   }
+
+   ~deferred_heap()
+   {
+      clear();
+   }
 
 private:
-    deferred_heap(std::size_t capacity) { heap.resize(capacity); }
+   deferred_heap(std::size_t capacity = 512UL) { heap.resize(capacity); }
 
-    std::vector<std::byte> heap;
-
-    //TODO make circular
-    std::queue<element_information> elements;
+   std::vector<std::byte> heap;
+   std::queue<element_information> elements;
 };
 
 template <typename type>
 class lazy_destruct
 {
 public:
-    using element_type = type;
-    using pointer = element_type*;
-    using reference = element_type&;
+   using element_type = type;
+   using pointer = element_type*;
+   using reference = element_type&;
 
-    template <typename... Args>
-    lazy_destruct(Args&&... args)
-    {
-        new (value) element_type{std::forward<Args&&>(args)...};
-    }
-    lazy_destruct(lazy_destruct&& other) : value{ std::move(other.value) } {}
-    ~lazy_destruct()
-    {
-        const auto deleter = [](std::byte* object) noexcept
-        {
-            reinterpret_cast<element_type*>(object)->~element_type();
-        };
-        deferred_heap::get().enqueue({sizeof(element_type), deleter}, value);
-    }
+   template <typename... Args>
+   lazy_destruct(Args&&... args)
+   {
+      new (value) element_type{std::forward<Args&&>(args)...};
+   }
+   lazy_destruct(lazy_destruct&& other) : value{ std::move(other.value) } {}
+   ~lazy_destruct()
+   {
+      const auto deleter = [](std::byte* object) noexcept
+      {
+         reinterpret_cast<element_type*>(object)->~element_type();
+      };
+      deferred_heap::get().enqueue({sizeof(element_type), deleter}, value);
+   }
 
-    reference operator*() { return *reinterpret_cast<element_type*>(value); }
-    const reference operator*() const { return *reinterpret_cast<const element_type*>(value); }
-    pointer operator->() { return reinterpret_cast<element_type*>(value); }
-    const pointer operator->() const { return reinterpret_cast<const element_type*>(value); }
+   reference operator*() { return *reinterpret_cast<element_type*>(value); }
+   const reference operator*() const { return *reinterpret_cast<const element_type*>(value); }
+   pointer operator->() { return reinterpret_cast<element_type*>(value); }
+   const pointer operator->() const { return reinterpret_cast<const element_type*>(value); }
 
 private:
-    std::byte value[sizeof(element_type)];
+   std::byte value[sizeof(element_type)];
 };
 
 class Noisy
